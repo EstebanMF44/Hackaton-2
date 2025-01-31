d6cb78c0c3b5d38fa6d388572c8a34fe2bea8178
Arya KHORSAND <arya.khorsand@etu.minesparis.psl.eu>
2025-01-31 14:11:56 +0100

    dijkstra

--- arya.py
+++ arya.py
@@ -2,6 +2,7 @@
 # Puzzle 8 : 'taquin'
 import numpy as np
 import random as rd
+from collections import deque
 
 goal = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
 
@@ -28,8 +29,8 @@ def remplissage_tableau():
 
 # Solveur
 # On renvoie les tableaux atteignables à partir d'une position après 1 seul déplacement du 0
-def atteignable(tableau):
-    l = []
+def generer_voisin(tableau):
+    LLL = []
     tableau1 = tableau.copy()  # un cran au dessus
     tableau2 = tableau.copy()  # un cran en dessous
     tableau3 = tableau.copy()  # un cran à gauche
@@ -44,26 +45,89 @@ def atteignable(tableau):
             position[1]
         ]  # le zéro est remplacé
         tableau1[position[0] - 1][position[1]] = 0  # le zéro remplace
-        l.append(tableau1)
+        LLL.append(tableau1)
     if (position[0] + 1) in [0, 1, 2]:
         tableau2[position[0]][position[1]] = tableau1[position[0] + 1][
             position[1]
         ]  # le zéro est remplacé
         tableau2[position[0] + 1][position[1]] = 0  # le zéro remplace
-        l.append(tableau2)
+        LLL.append(tableau2)
     if (position[1] - 1) in [0, 1, 2]:
         tableau3[position[0]][position[1]] = tableau1[position[0]][
             position[1] - 1
         ]  # le zéro est remplacé
         tableau3[position[0]][position[1] - 1] = 0  # le zéro remplace
-        l.append(tableau3)
+        LLL.append(tableau3)
     if (position[0] + 1) in [0, 1, 2]:
         tableau4[position[0]][position[1]] = tableau1[position[0]][
             position[1] + 1
         ]  # le zéro est remplacé
         tableau4[position[0]][position[1] + 1] = 0  # le zéro remplace
-        l.append(tableau4)
-    return l
+        LLL.append(tableau4)
+    return LLL
 
 
-# un exemple : print(atteignable(np.array([[2, 3, 4], [5, 0, 1], [8, 6, 7]])))
+# un exemple : print(generer_voisin(np.array([[2, 3, 4], [5, 0, 1], [8, 6, 7]])))
+
+
+def array_to_tuple(array):
+    """Convertit un np.array en tuple immuable pour utilisation comme clé de dictionnaire."""
+    return tuple(map(tuple, array))
+
+
+def tuple_to_array(tpl):
+    """Convertit un tuple en np.array pour affichage."""
+    return np.array(tpl)
+
+
+def bfs_find_goal(dico, goal):
+    goal_tuple = array_to_tuple(goal)
+    for start in dico:  # On ne sait pas où commence le parcours
+        queue = deque([(start, [start])])  # File d'attente avec chemin
+        visited = set()
+
+        while queue:
+            current, path = queue.popleft()
+            visited.add(current)
+
+            for neighbor in dico.get(current, []):
+                neighbor_tuple = array_to_tuple(neighbor)
+                if neighbor_tuple == goal_tuple:  # Si on trouve goal
+                    return path + [neighbor_tuple]
+                if neighbor_tuple not in visited:
+                    queue.append((neighbor_tuple, path + [neighbor_tuple]))
+                    visited.add(neighbor_tuple)
+
+    return None  # Si goal n'est pas trouvé
+
+
+def print_path(path):
+    """Affiche le chemin de manière lisible."""
+    if path:
+        print("Chemin trouvé :")
+        for step in path:
+            print(tuple_to_array(step))
+            print("↓")
+    else:
+        print("Aucun chemin trouvé.")
+
+
+# Exemple d'utilisation :
+dico = {
+    array_to_tuple(np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])): [
+        np.array([[0, 0, 1], [0, 0, 0], [0, 0, 0]]),
+        np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]]),
+    ],
+    array_to_tuple(np.array([[0, 0, 1], [0, 0, 0], [0, 0, 0]])): [
+        np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        np.array([[0, 0, 1], [0, 1, 0], [0, 0, 0]]),
+    ],
+    array_to_tuple(np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]])): [
+        np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0]]),
+    ],
+}
+
+goal = np.array([[0, 0, 1], [0, 1, 0], [0, 0, 0]])
+path = bfs_find_goal(dico, goal)
+print_path(path)
