629592afb43691942773e1834f1e824989d13c83
Pierre-Guillaume <pierre-guillaume.petitmangin@etu.minesparis.psl.eu>
2025-01-31 14:32:22 +0100

    aj

--- Background.py
+++
@@ -1,8 +0,0 @@
-fron .GameObject import GameObject
-class Background (GameObject) :
-    def __init__(self, name, position, size, background_type) -> None:
-        super(Background, self).__init__(name, position, size)
-        self.background_type = background_type
-    
-    def attach(self, game_object) -> None:
-        pass
