c9975dbc81ea65e26095f2886f416580d7c07261
Pierre-Guillaume <pierre-guillaume.petitmangin@etu.minesparis.psl.eu>
2025-01-31 15:45:20 +0100

    test

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-import felt as flt
+import flet as ft
+
+def main(page):
+    # Créer un carré rouge de 100x100 px
+    square = ft.Container(
+        width=100,
+        height=100,
+        bgcolor=ft.colors.RED
+    )
+    
+    # Ajouter le carré à la page
+    page.add(square)
+
+# Lancer l'application Flet
+ft.app(target=main)
+
+
